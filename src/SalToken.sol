db2ff78bc1b336c8bf7cb5a60687d8ac2894803b
Felipe Faria <felipeasf@gmail.com>
2025-03-10 16:50:48 -0300

    initial commit

---
+++ src/SalToken.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/access/AccessControlUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable@5.2.0/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/proxy/utils/UUPSUpgradeable.sol";
+
+/// @custom:security-contact luca@bambalabs.co
+contract SalToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address recipient, address defaultAdmin, address minter, address upgrader)
+        public initializer
+    {
+        __ERC20_init("SalToken", "SAL");
+        __ERC20Burnable_init();
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        _mint(recipient, 1000000000 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(UPGRADER_ROLE, upgrader);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
+}
